c3b580ffdf4a6fc64a2468e5449332b48810f7e8
Phillip M Overå <phillipo@stud.ntnu.no>
2024-11-28 01:31:53 +0100

    Added DynamoDB table to infrastructure

--- __main__.py
+++ __main__.py
@@ -6,3 +6,16 @@ bucket = aws.s3.Bucket("my-pulumi-bucket", bucket_prefix="pulumi-demo-", acl="pr
 
 # Export the bucket name
 pulumi.export("bucket_name", bucket.id)
+
+# Create a DynamoDB table
+table = aws.dynamodb.Table(
+    "my-dynamodb-table",
+    attributes=[
+        {"name": "id", "type": "S"}  # Define a string attribute named 'id'
+    ],
+    hash_key="id",  # Define 'id' as the hash key
+    billing_mode="PAY_PER_REQUEST"  # Use on-demand pricing
+)
+
+# Export the DynamoDB table name
+pulumi.export("dynamodb_table_name", table.name)
